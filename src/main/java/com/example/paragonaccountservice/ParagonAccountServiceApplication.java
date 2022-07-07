d1a2a7a84a1084b9fc115e850e86860191842805
Jotaro <zibble@mail.ru>
2022-07-07 13:11:00 +0400

    init

---
+++ src/main/java/com/example/paragonaccountservice/ParagonAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.paragonaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParagonAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParagonAccountServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paragonaccountservice/ParagonAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paragonaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParagonAccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
